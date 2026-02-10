787cbf1d3c298d651023dcbfc6c970d29a558c49
Kirill Goncharov <gka.post@yandex.ru>
2026-02-10 20:04:52 +0700

    II.1. ВЫЧИСЛЕНИЕ ЗНАЧЕНИЙ ФУНКЦИИ.

--- Lab_7/Lab_7.cpp
+++ Lab_7/Lab_7.cpp
@@ -1,14 +1,25 @@
 ﻿#include <cmath>  
 #include <iostream>
+#include <cstdio>  
 
 using namespace std;
 
 int main(){
     setlocale(LC_ALL, "Russian");
     //II.1.	ВЫЧИСЛЕНИЕ ЗНАЧЕНИЙ ФУНКЦИИ. 
-    double step;
-
-    step = 4.0/10.0;
-    cout << step;
+    double step,f,start;
+    start = 0.0;
+    printf("Введите число в диапозоне (0,4]\n");
+    cin >> start;
+    printf("");
+    step = start/10.0;
+    f = start;
+    for (int i = 0; i < 10; i++) {
+        double znach;
+        f += step;
+        znach = sin(f) / f;
+        printf("%f\n", znach);
+       
+    }
 }
 
