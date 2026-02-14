71ae6eab4cb7065981e298e0ddf4c3ea02957189
Kirill Goncharov <gka.post@yandex.ru>
2026-02-14 18:11:57 +0700

    Lab_№8

--- Lab_7/Lab_7.cpp
+++ Lab_7/Lab_7.cpp
@@ -1,109 +1,80 @@
 ﻿#include <cmath>  
 #include <iostream>
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable:4996) 
+
 using namespace std;
 
-int main(){
-    setlocale(LC_ALL, "Russian");
-    //II.1.	ВЫЧИСЛЕНИЕ ЗНАЧЕНИЙ ФУНКЦИИ. 
-    double step,f,start;
-    printf("Введите число в диапозоне (0,4]\n");
-    scanf("%lf", &start);
-    
-    if (start > 4.0 || start <= 0.0) {
-        printf("Выход за диапозон");
-        return 0;
-    }
-    else {       
-    }
+#define Dl 10
 
-    printf("");
-    step = (4.0 - start)/10.0;
-    f = start + step;
-    for (int i = 0; i < 10; i++) {
-        double znach;
-        f += step;
-        znach = sin(f) / f;
-        printf("%f\n", znach);
-       
-    }
-    //II.2.	ЧИСЛА ФИБОНАЧЧИ
-    int Max_value;
+int main() {
+    setlocale(LC_ALL, "RU");
+    srand(time(NULL));
 
-    printf("Введите максимально допустимую сумму\n");
-    scanf("%d", &Max_value);
 
-    if (Max_value < 0) {
-        printf("Недопустимое значение");
-        return 0;
+    cout << "Выберите вариант заполнения массива" << endl;
+    cout << "1.статический массив, индексная адресация;" << endl;
+    cout << "2.статический массив, адресация с помощью указателя" << endl;
+    cout << "3.динамический массив, индексная адресация;" << endl;
+    cout << "4.динамический массив, адресация с помощью указателя " << endl;
 
-    }
-    else {
-        if (Max_value == 0) {
-            printf("N = 1, S = 0\n");
-        }
-        else if (Max_value == 1) {
-            printf("N = 2, S = 1\n");
-        }
-        else {
-            int a_1, a_2, a_3;
-            int sum;
-            int col;
-            a_1 = 0;
-            a_2 = 1;
-            a_3 = 0;
-            sum = 0;
-            col = 1;
-            while (sum <= Max_value) {
-                sum += a_2;
-                a_3 = a_1 + a_2;
-                a_1 = a_2;
-                a_2 = a_3;
-                col += 1;
-            }
-            
-            printf("N = %d S = %d\n", col, sum);
+    int num;
+    cin >> num;
 
-        }
+    cout << endl;
+    if (num == 1) {
         
+        int mas[Dl];
+        for (int i = 0; i < Dl; i++) {
+            mas[i] = 1 + rand() % 100;
+        }
+        for (int i = 0; i < Dl; i++) {
+            cout << mas[i] << " ";
+        }
+        cout << endl;
     }
-    //П.3. КРЕДИТНЫЕ ПЛАТЕЖИ. 
-    int procent, sum_kredi;
-    int time;
-    printf("Введите сумму кредита\n");
-    scanf("%d", &sum_kredi);
+    else if (num == 2) {
+        int mas[Dl];
+        int* el;
+        el = mas;
 
-    printf("срок\n");
-    scanf("%d", &time);
-    
-    printf("Процентная ставка\n");
-    scanf("%d", &procent);
-
-    double month_pro = (static_cast<double> (procent) / static_cast<double> (time)) / 100;
-    double all_procent;
-
-
-    printf("---------------------------------------\n");
-    printf("\t Долг\t\t Процент\t\t Платеж\n");
-    printf("---------------------------------------\n");
-    double sum_kredit = static_cast<double> (sum_kredi);
-    double month_pay = (sum_kredit / time);
-    all_procent = 0.0;
-    for (int i = 1; i <= time; i++) {
-        double pay,yet_pay;
-        pay = static_cast<double>(sum_kredit) * month_pro;
-        pay = round(pay * 100) / 100;
-        yet_pay = month_pay + pay;
-        printf("%d\t%.2f\t%.2f\t\t%.2f\n",i, static_cast<double>(sum_kredit), pay, yet_pay);
-        sum_kredit += pay;
-        sum_kredit -= yet_pay;
-        all_procent += pay;
-
+        for (int i = 0; i < Dl; i++) {
+            *el = 1 + rand() % 100;
+            el++;
+        }
+        for (int i = 0; i < Dl; i++) {
+            cout << mas[i] << " ";
+        }
+        cout << endl;
     }
-    printf("---------------------------------------\n");
-    printf("Всего процентов: %.2f", all_procent);
+    else if (num == 3) {
+        int* mas;
+        mas = new int[Dl];
+
+        for (int i = 0; i < Dl; i++) {
+            mas[i] = 1 + rand() % 100;
+        }
+        for (int i = 0; i < Dl; i++) {
+            cout << mas[i] << " ";
+        }
+        cout << endl;
+        delete[] mas;
+    }
+    else if(num == 4){
+        int* mas, *el;
+        mas = new int[Dl];
+        el = mas;
+
+        for (int i = 0; i < Dl; i++) {
+            *el = 1 + rand() % 100;
+            el++;
+        }
+        for (int i = 0; i < Dl; i++) {
+            cout << mas[i] << " ";
+        }
+        cout << endl;
+
+        delete[] mas;
+    }
 
 
+    return 0;
 }
-
